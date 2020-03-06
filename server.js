0f6d51d9521e8ba2ad635441c567b14b9bb3c107
mekaleka <www.no-reply.com>
2020-03-05 19:27:22 -0700

    linked server.js backend with fron end code commented server.js

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+//Requireing dependencies
 const express = require("express");
+//uuid is the unique identifier for creating the unique id.  
 const uuid = require("uuid").v4;
+//Used for handling and transforming file paths. 
 const path = require("path");
+//Logger allows dev to see traffic coming and going. 
 const logger = require("morgan");
+//responsible for all asynchronous and synchronous file operations.  
 const fs = require("fs");
+//Uses callback funtions (req, res) and renders html elements based on passing arguments. 
 const app = express();
 
 // var userNotes = require("db/db.js")
 const PORT = process.env.PORT || 8080;
 var dataNotes = [];
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,38 +22,36 @@ app.use(express.static("public"));
 
 app.use(logger("dev"));
 
-// API Routes
+// API Routes - gets the file from index.js and reads the file with fs.readfile.
 app.get("/api/notes", function(req, res) {
   fs.readFile("db/db.json", "utf8", function(err, data) {
+    //Concatenates dataNotes to the array.
     dataNotes = [].concat(JSON.parse(data));
     res.json(JSON.parse(data));
   });
 });
-
+// Returns the API notes and pushes to to the newNote array.
 app.post("/api/notes", function(req, res) {
   const newNote = { id: uuid(), ...req.body };
-  // var newData = [...dataNotes, newNote]
   dataNotes.push(newNote);
-  // res.redirect
   fs.writeFile("db/db.json", JSON.stringify(dataNotes), function(
+    //call back function for when data is loaded .
     err,
     data
   ) {
     console.log(err, data);
+    //sends the response to newNote
     res.send(newNote);
   });
 });
-
+//function that returns a response of "note not found" if dataNotes does not contain a note.
 app.delete("/api/notes/:id", function(req, res) {
+  //if dataNotes
   var note = dataNotes.find(i => i.id === req.params.id);
   if (!note) return res.send("note not found");
   var index = dataNotes.indexOf(note);
   dataNotes.splice(index, 1);
-  // dataNotes => dataNotes.filter (i => i.id != req.params.id)
-  fs.writeFile("db/db.json", JSON.stringify(dataNotes), function(
-    err,
-    data
-  ) {
+  fs.writeFile("db/db.json", JSON.stringify(dataNotes), function(err, data) {
     console.log(err, data);
     res.send(true);
   });
@@ -54,9 +59,12 @@ app.delete("/api/notes/:id", function(req, res) {
 
 // HTML Routes
 app.get("/notes", function(req, res) {
+  //function requests a response to get a string from notes.
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+//
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+//listens for an assigned port otherwise port 8080 defined at the top.
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
